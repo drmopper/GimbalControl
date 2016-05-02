04c24f3626900460281ba20a1f3214aeaec3c109
drmopper <xi-chen-1@uiowa.edu>
2016-05-02 13:10:22 -0500

    added stuff to demoBoard1

--- demoBoard1/demoBoard1.ino
+++ demoBoard1/demoBoard1.ino
@@ -18,6 +18,9 @@
 #define YawHalt 90
 #define YawCCW 48
 #define YawCW 138
+#define YawCCWCycleTime 10120
+#define YawCWCycleTime 10120
+
 
 SimpleTimer timer;
 Servo pitchServo;
@@ -26,20 +29,34 @@ Servo yawServo;
 const int pitchMap[] PROGMEM= {46,47,47,48,48,49,49,50,50,51,51,52,52,53,54,54,55,55,56,56,57,57,58,58,59,60,60,61,61,62,62,63,63,64,64,65,65,66,66,67,68,68,69,69,70,70,71,71,72,72,73,74,74,75,75,76,76,77,77,78,78,79,80,80,81,81,82,82,83,83,84,84,85,85,86,87,87,88,88,89,89,90,90,91,91,92,93,93,94,94,95,95,96,96,97,97,98,98,99,100,100,101,101,102,102,103,103,104,104,105,105,106,107,107,108,108,109,109,110,110,111,111,112,113,113,114,114,115,115,116,116,117,117,118,118,119};
 int currentPitch;
 int currentYaw;
+int inputPitch;
+int inputYaw;
+boolean shouldReset;
 
 void setup() {
   Serial.begin(9600);
   pitchServo.attach(PitchPin);
   yawServo.attach(YawPin);
-  currentPitch = 0;
-  currentYaw = 0;
-  pitchServo.write(pitchMap[currentPitch+90]);//set pitch position to 90
+  currentPitch = 0;//zero points straight down
+  currentYaw = 0;//zero points straight forward
+  inputPitch = 0;
+  inputYaw = 0;
+  pitchServo.write(pitchMap[currentPitch]);//set pitch position to 90
   yawServo.write(YawHalt);//halt any yaw movement
 }
 
 void loop() {
-  // put your main code here, to run repeatedly:
-
+  //wait for input and read input from serial
+  //extract info and parse to Int to inputPitch, inputYaw, and shouldReset
+  //using readInput()
+  turnPitch();
+  turnYaw();
+  currentPitch = inputPitch;
+  currentYaw = inputYaw;
+  if(shouldReset){
+    resetMode();
+  }
+  shouldReset = false;
 }
 
 void readInput(){
@@ -52,6 +69,59 @@ void resetMode(){
   delay(5000);
   digitalWrite(BoardTwo,LOW);
   Serial.println(F("Mode Reset Complete"));
+}
+
+void turnPitch(){
+  if(inputPitch!=currentPitch){
+    pitchServo.write(pitchMap[inputPitch]);
+  }
+}
+
+void turnYaw(){
+  if(inputYaw>=currentYaw){
+    if((inputYaw-currentYaw)<=180){
+      turnYawCW(inputYaw-currentYaw);
+    }
+    else{
+      turnYawCCW(360-inputYaw-currentYaw);
+    }
+  }
+  else{
+    if((currentYaw-inputYaw)<=180){
+      turnYawCCW(currentYaw-inputYaw);
+    }
+    else{
+      turnYawCW(360-inputYaw-currentYaw);
+    }
+  }
+}
+
+void turnYawCW(int degree){
+  if((currentYaw + degree)>360){
+    resetMode();
+    yawServo.write(YawCW);
+    delay((currentYaw+degree-360)*YawCWCycleTime*1000/360);
+    yawServo.write(YawHalt);
+  }
+  else{
+    yawServo.write(YawCW);
+    delay(degree*YawCWCycleTime*1000/360);
+    yawServo.write(YawHalt);
+  }
+}
+
+void turnYawCCW(int degree){
+  if((currentYaw-degree)<0){
+    resetMode();
+    yawServo.write(YawCCW);
+    delay((degree-currentYaw)*YawCCWCycleTime*1000/360);
+    yawServo.write(YawHalt);
+  }
+  else{
+    yawServo.write(YawCCW);
+    delay(degree*YawCCWCycleTime*1000/360);
+    yawServo.write(YawHalt);
+  }
 }
 
 
